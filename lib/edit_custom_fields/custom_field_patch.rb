278443de86a40f493854ca6511ad6d04ab0fc524
Moritz Gschwandtner <gschwandtner@artech.at>
2024-06-19 13:31:18 +0200

    fix(redmine 5): create nested dirs for nested modules.
    this is required for zeitwerks autoload mechanism.

--- lib/edit_custom_fields/custom_field_patch.rb
+++ lib/edit_custom_fields/custom_field_patch.rb
@@ -20,8 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-require_dependency 'custom_field'
-
 module EditCustomFields
   module CustomFieldPatch
     unloadable
